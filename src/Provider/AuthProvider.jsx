1688b44aac9aa67c1e39734c110e9df1efd024e8
Abdullah3d <abdullah.ibrarhim.609@gmail.com>
2024-04-19 17:21:48 +0600

    added update profile

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,4 +1,4 @@
-import { GithubAuthProvider, GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+import { GithubAuthProvider, GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { createContext, useEffect, useState } from "react";
 import app from "../firebase/firebase.confie";
 
@@ -9,56 +9,63 @@ const googleProvider = new GoogleAuthProvider();
 const githubProvider = new GithubAuthProvider();
 
 const auth = getAuth(app);
-const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
-    const [loading , setLoading] = useState(true);
+    const [loading, setLoading] = useState(true);
 
-    const createUser = (email, password) =>{
+    const createUser = (email, password) => {
         setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
-    const signIn = (email, password) =>{
+    const updateUserProfile = (name , photo) => {
+        return updateProfile(auth.currentUser, {
+            displayName: name, photoURL: photo
+        })
+    }
+
+    const signIn = (email, password) => {
         setLoading(true);
         return signInWithEmailAndPassword(auth, email, password);
     }
 
-    const signInWithGoogle = () =>{
+    const signInWithGoogle = () => {
         setLoading(true)
-        return signInWithPopup(auth, googleProvider );
+        return signInWithPopup(auth, googleProvider);
 
     }
-    const signInWithGithub = () =>{
+    const signInWithGithub = () => {
         setLoading(true)
         return signInWithPopup(auth, githubProvider);
     }
 
-    const logOut = () =>{
+    const logOut = () => {
         setLoading(true);
         return signOut(auth);
     }
-    useEffect(() =>{
-         const unSubscribe = onAuthStateChanged(auth, currentUser =>{
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
             console.log('user in the auth state change', currentUser);
             setUser(currentUser);
             setLoading(false);
         });
-        return () =>{
+        return () => {
             unSubscribe();
         }
-    },[])
+    }, [])
     const authInfo = {
         user,
         loading,
         createUser,
         signInWithGoogle,
         signInWithGithub,
+        updateUserProfile,
         signIn,
         logOut
     }
     return (
         <AuthContext.Provider value={authInfo}>
-             {children}
+            {children}
         </AuthContext.Provider>
     );
 };
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,59 @@
+import { useContext } from "react";
 import { Helmet } from "react-helmet-async";
+import { AuthContext } from "../Provider/AuthProvider";
+import Footer from "./Footer";
+import { useNavigate } from "react-router-dom";
 
 const Profile = () => {
+    const navigate = useNavigate();
+    const { user, createUser, updateUserProfile } = useContext(AuthContext);
+    const handleUpdate = e => {
+        e.preventDefault();
+        const form = new FormData(e.currentTarget);
+        const name = form.get('name');
+        const photo = form.get('photo');
+        console.log(name, photo);
+
+        createUser(name, photo)
+            .then(result => {
+                console.log(result.user)
+                updateUserProfile(name, photo)
+                    .then(() => {
+                        navigate(form);
+                    })
+
+
+            })
+            .catch(error => {
+                console.error(error)
+            })
+
+    }
     return (
         <div>
             <Helmet>
                 <title>Task-9 | profile </title>
             </Helmet>
-            <h2 className="text2xl">Hello from update profile</h2>
+            <form onSubmit={handleUpdate} className=" mb-10md:w-3/4 lg:w-1/2 mx-auto">
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Name</span>
+                    </label>
+                    {user?.name || <input type="text" name="name" placeholder="name" className="input input-bordered" required />}
+                </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Photo URl</span>
+                    </label>
+                    {
+                        user?.photo || <input type="text" name="Photo" placeholder="Photo URL" className="input input-bordered" required />
+                    }
+                </div>
+                <div className="form-control mt-6">
+                    <button className="btn btn-primary">Register</button>
+                </div>
+            </form>
+            <Footer></Footer>
         </div>
     );
 };
