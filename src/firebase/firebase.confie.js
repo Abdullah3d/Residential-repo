8ac53e3d38101273d9c1810b991d755d9a2645de
Abdullah3d <abdullah.ibrarhim.609@gmail.com>
2024-04-19 18:11:05 +0600

    upadte enviorment

--- src/firebase/firebase.confie.js
+++ src/firebase/firebase.confie.js
@@ -4,13 +4,16 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+console.log('inside firebase config', import.meta.env.VITE_PASS)
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCqXfMc3s2AUS6im69kQvbCMeXwd4OR4ig",
-    authDomain: "assignment-9-286dd.firebaseapp.com",
-    projectId: "assignment-9-286dd",
-    storageBucket: "assignment-9-286dd.appspot.com",
-    messagingSenderId: "582055319681",
-    appId: "1:582055319681:web:bf2488dc4170a36025d580"
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
